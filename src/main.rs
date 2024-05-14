302a1de07f25d802dfab64cbefea593155d4da62
Sebastien LELEU <leleu.sebastien@hotmail.fr>
2024-05-14 17:04:06 +0200

    add sub and scl for vector

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,49 @@
-use std::ops::Add;
+const GREEN: &str = "\x1b[1;32m";
+const CYAN: &str = "\x1b[1;36m";
+const END: &str = "\x1b[0m";
+const YELLOW: &str = "\x1b[1;33m";
+
+/* VECTOR */
+
+use std::ops::{ Add,
+                Sub,
+                Mul};
 use std::fmt;
 
 struct Vector<K> {
     data: Vec<K>,
 }
 
-
 // add
-impl<K> Vector<K> where K: Add<Output = K> + Copy {
+impl<K> Vector<K>
+where K: Add<Output = K> + Copy {
     fn add(&mut self, v: &Vector<K>) {
         for (a, b) in self.data.iter_mut().zip(&v.data) {
             *a = *a + *b;
         }
     }
 }
+
+// sub
+impl<K> Vector<K>
+where K: Sub<Output = K> + Copy {
+    fn sub(&mut self, v: &Vector<K>) {
+        for (a, b) in self.data.iter_mut().zip(&v.data) {
+            *a = *a - *b;
+        }
+    }
+}
+
+//scl
+impl<K> Vector<K>
+where K: Mul<Output = K> + Copy {
+    fn scl(&mut self, scalar: &K) {
+        for a in self.data.iter_mut() {
+            *a = *a * *scalar;
+        }
+    }
+}
+
 
 // From trait
 impl<K, const N: usize> From<[K; N]> for Vector<K> where K: Copy {
@@ -37,11 +67,46 @@ impl<K: fmt::Display + std::fmt::Debug> fmt::Display for Vector<K> {
     }
 }
 
-fn main() {
-    let mut u = Vector::from([2.,3.,5.]);
-    let v = Vector::from([5., 7., 5.]);
+fn vector_tests() {
+    println!("{GREEN}============= ADD ============={END}");
+
+    println!("{YELLOW}Example tests{END}");
+    let mut u = Vector::from([2.,3.]);
+    let v = Vector::from([5., 7.]);
     u.add(&v);
-    println!("{}", u);
+    println!("{CYAN}{}{END}", u);
     // [7.0]
     // [10.0]
+
+    println!("{GREEN}============= SUB ============={END}");
+
+    println!("{YELLOW}Example tests{END}");
+    let mut u = Vector::from([2., 3.]);
+    let v = Vector::from([5., 7.]);
+    u.sub(&v);
+    println!("{CYAN}{}{END}", u);
+    // [-3.0]
+    // [-4.0]
+
+    println!("{GREEN}============= SCL ============={END}");
+
+    println!("{YELLOW}Example tests{END}");
+    let mut u = Vector::from([2., 3.]);
+    u.scl(&2.);
+    println!("{CYAN}{}{END}", u);
+    // [4.0]
+    // [6.0]
+
+}
+
+fn matrix_tests() {
+
+}
+
+fn main() {
+    println!("\n\n{GREEN}   RUN VECTOR TESTS    {END}\n");
+    vector_tests();
+
+    println!("\n\n{GREEN}   RUN MATRIX TESTS    {END}\n");
+    matrix_tests();
 }
