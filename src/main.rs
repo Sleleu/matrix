cdc58638c299f005ad55c0032ffe0e48b7a98f11
sleleu <leleu.sebastien@hotmail.fr>
2024-05-16 21:18:17 +0200

    add dot product

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,41 @@ use matrix::Matrix;
 mod linear_interpolation;
 use crate::linear_interpolation::lerp;
 
+fn dot_product_test() {
+
+    println!("\n\n{GREEN}   DOT PRODUCT TESTS    {END}\n");
+
+    println!("{YELLOW}Example tests{END}");
+    let u: Vector<f64> = Vector::from([0., 0.]);
+    let v: Vector<f64> = Vector::from([1., 1.]);
+    println!("{:?}", u.dot(&v));
+    // 0.0
+    let u: Vector<f64> = Vector::from([1., 1.]);
+    let v: Vector<f64> = Vector::from([1., 1.]);
+    println!("{:?}", u.dot(&v));
+    // 2.0
+    let u: Vector<f64> = Vector::from([-1., 6.]);
+    let v: Vector<f64> = Vector::from([3., 2.]);
+    println!("{:?}", u.dot(&v));
+    // 9.0
+
+    println!("{YELLOW}other tests{END}");
+    print!("Different size of vector: ");
+    let u: Vector<f64> = Vector::from([10., 2., 1., 10.]);
+    let v: Vector<f64> = Vector::from([5., 1.]);
+    println!("{:?}", u.dot(&v));
+
+    print!("Empty vector: ");
+    let u: Vector<f64> = Vector::from([]);
+    let v: Vector<f64> = Vector::from([]);
+    println!("{:?}", u.dot(&v));
+
+    print!("int vector: ");
+    let u: Vector<i32> = Vector::from([3, 3]);
+    let v: Vector<i32> = Vector::from([2, 2]);
+    println!("{:?}", u.dot(&v));
+}
+
 fn linear_interpolation_test() {
 
     println!("\n\n{GREEN}   LINEAR INTERPOLATION TESTS    {END}\n");
@@ -162,4 +197,5 @@ fn main() {
 
     linear_combination_test();
     linear_interpolation_test();
+    dot_product_test();
 }
