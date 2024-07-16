02c12e5b91fe7e2da6cd0c8792ca78454acdde49
Sebastien LELEU <leleu.sebastien@hotmail.fr>
2024-07-16 18:58:35 +0200

    finish ex13

--- src/lib/matrix/rank.rs
+++ src/lib/matrix/rank.rs
@@ -1,12 +1,21 @@
 use crate::matrix::model::Matrix;
-use std::{ops::{Add, Div, Mul, Sub}, usize};
+use std::ops::{Add, Div, Mul, Sub};
 
 impl<K> Matrix<K>
-where K: Add<Output = K> + Sub<Output = K> + Mul<Output = K> + Div<Output = K> + Default
+where K: Add<Output = K> + Sub<Output = K> + Mul<Output = K> + Div<Output = K> + Default + Clone + Copy + PartialEq
 {
     pub fn rank(&self) -> usize {
-        let rank: usize = 42;
+        let row_echelon_matrix: Matrix<K> = self.row_echelon();
+        let mut rank: usize = 0;
 
+        for row in row_echelon_matrix.data.iter() {
+            for i in row.iter() {
+                if *i != K::default() {
+                    rank += 1;
+                    break;
+                }
+            }
+        }
         rank
     }
 }
--- tests/ex13/ex13.rs
+++ tests/ex13/ex13.rs
@@ -27,6 +27,60 @@ fn rank_test() {
         ]);
     println!("{}", u.rank());
     // 3
+
+    println!("\n\n{GREEN} OTHER TESTS {END}\n");
+
+    println!("\n{CYAN}Second column consists of zeros :{END}");
+    let u = Matrix::from([
+        [ 8., 0., -2.],
+        [ 4., 0., 20.],
+        [ 7., 0., 1.],
+        [ 1., 0., 7.],
+        ]);
+    println!("{}", u.rank());
+    // 2
+
+    println!("\n{CYAN}Only zeros :{END}");
+    let u = Matrix::from([
+        [ 0., 0., 0.],
+        [ 0., 0., 0.],
+        [ 0., 0., 0.],
+        [ 0., 0., 0.],
+        ]);
+    println!("{}", u.rank());
+    // 0
+
+
+    println!("\n{CYAN}Same example as wikipedia (https://fr.wikipedia.org/wiki/Rang_(alg%C3%A8bre_lin%C3%A9aire)) :{END}");
+    let u = Matrix::from([
+        [ 1., 0., 2., 3.],
+        [ 2., 0., 4., 6.],
+        [ 0., 2., 2., 0.],
+        [ 1., 2., 4., 3.],
+        ]);
+    println!("{}", u.rank());
+    // 2
+
+    println!("\n{CYAN}Random values, the system has a unique solution :{END}");
+    let u = Matrix::from([
+        [ 15345., 0.4, 23123134535.44475, -3213123355345.73501, 5.],
+        [ 265433., 0.1, 4453453453131.47575, 631235543543.124770477, 4.],
+        [ 0.1, 2343443.41404, 23213455345., 01.4775140, 8.],
+        [ 11016894414.41014, -2972101.4140411, 4435389., -3312344.0107575404, 3.],
+        ]);
+    println!("{}", u.rank());
+    // 4
+
+    println!("\n{CYAN}(l1, l2, l3) are linearly dependant :{END}");
+    let u = Matrix::from([
+        [1., 2., 8.],
+        [2., 4., 16.],
+        [3., 6., 24.],
+        ]);
+    println!("{}", u.rank());
+    // 1
+
+
 }
 
 fn main () {
