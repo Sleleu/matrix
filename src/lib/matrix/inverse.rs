b8bd9a5754a5d8fcb892421241b40c5fd963ef18
Sebastien LELEU <leleu.sebastien@hotmail.fr>
2024-07-16 17:17:05 +0200

    add col / row method + better ex12

--- src/lib/matrix/inverse.rs
+++ src/lib/matrix/inverse.rs
@@ -5,46 +5,42 @@ use std::convert::From;
 impl<K> Matrix<K>
 where K: From<i32> + std::ops::SubAssign + Add<Output = K> + Sub<Output = K> + Mul<Output = K> + Div<Output = K> + PartialEq + Copy + Default + Neg<Output = K> {
     
-    fn get_identity(&self, col: usize, row: usize) -> Matrix<K> {
+    fn get_identity(&self) -> Matrix<K> {
 
-        let mut identity : Matrix<K> = Matrix { data: vec![vec![K::default(); col]; row] };
+        let mut identity : Matrix<K> = Matrix { data: vec![vec![K::default(); self.col()]; self.row()] };
 
-        for i in 0 .. col {
+        for i in 0 .. self.col() {
             identity.data[i][i] = 1.into()
         }
         identity
     }
     
     pub fn inverse(&mut self) -> Result<Matrix<K>, &str> {
-        let mut pivot_index: usize = 0;
-        let row: usize = self.data.len();
-        let col: usize = self.data[0].len();
-        let det: K = self.determinant(); // si det 0 pas d'inverse, a faire
 
-        if det == K::default() {
+        let mut pivot_index: usize = 0;
+        if self.determinant() == K::default() || self.col() != self.row() {
             return Err("The matrix is not invertible");
         }
+        let mut identity: Matrix<K> = self.get_identity();
 
-        let mut identity: Matrix<K> = self.get_identity(col, row);
-
-        for r in 0..row {
-            if pivot_index >= col {
+        for r in 0..self.row() {
+            if pivot_index >= self.col() {
                 break;
             }
 
             let mut pivot_row: usize = r;
             while self.data[pivot_row][pivot_index] == K::default() {
                 pivot_row += 1;
-                if pivot_row == row {
+                if pivot_row == self.row() {
                     pivot_row = r;
                     pivot_index += 1; 
-                    if pivot_index == col {
+                    if pivot_index == self.col() {
                         break;
                     }
                 }
             }
 
-            if pivot_index == col {
+            if pivot_index == self.col() {
                 break;
             }
 
@@ -53,16 +49,16 @@ where K: From<i32> + std::ops::SubAssign + Add<Output = K> + Sub<Output = K> + M
 
             let pivot_value = self.data[r][pivot_index];
             if pivot_value != K::default() {
-                for j in 0..col {
+                for j in 0..self.col() {
                     identity.data[r][j] = identity.data[r][j] / pivot_value;
                     self.data[r][j] = self.data[r][j] / pivot_value;
                 }
             }
 
-            for i in 0..row {
+            for i in 0..self.row() {
                 if i != r && self.data[i][pivot_index] != K::default() {
                     let factor: K = self.data[i][pivot_index];
-                    for k in 0..col {
+                    for k in 0..self.col() {
                         self.data[i][k] = self.data[i][k] - factor * self.data[r][k];
                         identity.data[i][k] =  identity.data[i][k] - factor * identity.data[r][k];
                     }
--- src/lib/matrix/model.rs
+++ src/lib/matrix/model.rs
@@ -2,3 +2,13 @@
 pub struct Matrix<K> {
     pub data: Vec<Vec<K>>
 }
+
+impl <K> Matrix<K>  {
+    pub fn row(&self) -> usize {
+        self.data.len()
+    }
+
+    pub fn col(&self) -> usize {
+        self.data[0].len()
+    }
+}
