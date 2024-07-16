70e4c39d98fc3bda1033e8423a47321e0a90bb4f
Sebastien LELEU <leleu.sebastien@hotmail.fr>
2024-07-16 16:47:11 +0200

    functionnal inverse

--- src/lib/matrix/inverse.rs
+++ src/lib/matrix/inverse.rs
@@ -1,15 +1,76 @@
 use crate::matrix::model::Matrix;
-use std::ops::{Add, Sub, Mul, Div, Neg};
+use std::ops::{Add, Div, Mul, Neg, Sub};
+use std::convert::From;
 
 impl<K> Matrix<K>
-where K: Add<Output = K> + Sub<Output = K> + Mul<Output = K> + Div<Output = K> + PartialEq + Copy + Default + Neg<Output = K> {
-    pub fn inverse(&self) -> Matrix<K> {
+where K: From<i32> + std::ops::SubAssign + Add<Output = K> + Sub<Output = K> + Mul<Output = K> + Div<Output = K> + PartialEq + Copy + Default + Neg<Output = K> {
+    
+    fn get_identity(&self, col: usize, row: usize) -> Matrix<K> {
+
+        let mut identity : Matrix<K> = Matrix { data: vec![vec![K::default(); col]; row] };
+
+        for i in 0 .. col {
+            identity.data[i][i] = 1.into()
+        }
+        identity
+    }
+    
+    pub fn inverse(&mut self) -> Result<Matrix<K>, &str> {
+        let mut pivot_index: usize = 0;
         let row: usize = self.data.len();
         let col: usize = self.data[0].len();
         let det: K = self.determinant(); // si det 0 pas d'inverse, a faire
 
-        let inverse: Matrix<K> = Matrix { data: vec![vec![K::default(); row]; col] }; // utiliser inverse.row_echelon et recuperer la matrice identite transformee en matrice inverse
+        if det == K::default() {
+            return Err("The matrix is not invertible");
+        }
 
-        inverse
+        let mut identity: Matrix<K> = self.get_identity(col, row);
+
+        for r in 0..row {
+            if pivot_index >= col {
+                break;
+            }
+
+            let mut pivot_row: usize = r;
+            while self.data[pivot_row][pivot_index] == K::default() {
+                pivot_row += 1;
+                if pivot_row == row {
+                    pivot_row = r;
+                    pivot_index += 1; 
+                    if pivot_index == col {
+                        break;
+                    }
+                }
+            }
+
+            if pivot_index == col {
+                break;
+            }
+
+            self.data.swap(r, pivot_row);
+            identity.data.swap(r, pivot_row);
+
+            let pivot_value = self.data[r][pivot_index];
+            if pivot_value != K::default() {
+                for j in 0..col {
+                    identity.data[r][j] = identity.data[r][j] / pivot_value;
+                    self.data[r][j] = self.data[r][j] / pivot_value;
+                }
+            }
+
+            for i in 0..row {
+                if i != r && self.data[i][pivot_index] != K::default() {
+                    let factor: K = self.data[i][pivot_index];
+                    for k in 0..col {
+                        self.data[i][k] = self.data[i][k] - factor * self.data[r][k];
+                        identity.data[i][k] =  identity.data[i][k] - factor * identity.data[r][k];
+                    }
+                }
+            }
+            pivot_index += 1;
+        }
+
+        Ok(identity)
     }
 }
--- tests/ex12/ex12.rs
+++ tests/ex12/ex12.rs
@@ -3,35 +3,106 @@ use matrix::matrix::Matrix;
 
 fn inverse_test() {
 
-    println!("\n\n{GREEN} DETERMINANT TESTS {END}\n");
-    let u = Matrix::from([
+    println!("\n\n{GREEN} INVERSE TESTS {END}\n");
+    let mut u = Matrix::from([
     [1., 0., 0.],
     [0., 1., 0.],
     [0., 0., 1.],
     ]);
-    println!("{}", u.inverse());
+    match u.inverse() {
+        Ok(inverse) => println!("{}", inverse),
+        Err(e) => eprintln!("{}\n",e),
+    }
     // [1.0, 0.0, 0.0]
     // [0.0, 1.0, 0.0]
     // [0.0, 0.0, 1.0]
-    let u = Matrix::from([
+    let mut u = Matrix::from([
     [2., 0., 0.],
     [0., 2., 0.],
     [0., 0., 2.],
     ]);
-    println!("{}", u.inverse());
+    match u.inverse() {
+        Ok(inverse) => println!("{}", inverse),
+        Err(e) => eprintln!("{}\n",e),
+    }
     // [0.5, 0.0, 0.0]
     // [0.0, 0.5, 0.0]
     // [0.0, 0.0, 0.5]
-    let u = Matrix::from([
+    let mut u = Matrix::from([
     [8., 5., -2.],
     [4., 7., 20.],
     [7., 6., 1.],
     ]);
-    println!("{}", u.inverse());
+    match u.inverse() {
+        Ok(inverse) => println!("{}", inverse),
+        Err(e) => eprintln!("{}\n",e),
+    }
     // [0.649425287, 0.097701149, -0.655172414]
     // [-0.781609195, -0.126436782, 0.965517241]
     // [0.143678161, 0.074712644, -0.206896552]
 
+    println!("\n\n{GREEN} OTHER TESTS {END}\n"); 
+    let mut u = Matrix::from([
+        [2., 50., 6., 71., 8.],
+        [4., 2., 0., 42., 1.,],
+        [6., 12., 2., 1., 10.],
+        [8., 1., 20., 1., 10.],
+        [10., 0., 2., 1., 10.],
+        ]);
+        match u.inverse() {
+            Ok(inverse) => println!("{}", inverse),
+            Err(e) => eprintln!("{}\n",e),
+        }
+    // [0.2015356112160197][-0.33765632237146964][-0.7790376598852772][-0.05301599743470999][0.704590800584318]
+    // [0.06717853707200651][-0.11255210745715649][-0.1763458866284256][-0.01767199914490332][0.15153026686143925]
+    // [0.01866070474222396][-0.0312644742936543][-0.07676274628567364][0.05064666690419354][0.01431396301706631]
+    // [-0.017547297538034065][0.05326540064844847][0.06382940820180306][0.004881177183168862][-0.05999928741938949]
+    // [-0.203513022410661][0.33858267716535545][0.7880072683222311][0.04239854633555437][-0.601453664445792]
+
+    let mut u = Matrix::from([
+        [1., 1., 1., 1., 1.],
+        [1., 1., 1., 1., 1.,],
+        [1., 1., 1., 1., 1.],
+        [1., 1., 1., 1., 1.],
+        [1., 1., 0., 1., 1.],
+        ]);
+        match u.inverse() {
+            Ok(inverse) => println!("{}", inverse),
+            Err(e) => eprintln!("{}\n",e),
+        }
+    // The matrix is not invertible
+
+    let mut u = Matrix::from([
+        [8., 5., -2.],
+        [0., 0., 0.],
+        [7., 6., 1.],
+        ]);
+        match u.inverse() {
+            Ok(inverse) => println!("{}", inverse),
+            Err(e) => eprintln!("{}\n",e),
+        }
+    // The matrix is not invertible
+
+    let mut u = Matrix::from([
+        [8., 5.],
+        [1., 0.],
+        [7., 6.],
+        ]);
+        match u.inverse() {
+            Ok(inverse) => println!("{}", inverse),
+            Err(e) => eprintln!("{}\n",e),
+        }
+    // undefined
+
+    let mut u = Matrix::from([
+        [8., 1., 2.],
+        [1., 0., 4.],
+        ]);
+        match u.inverse() {
+            Ok(inverse) => println!("{}", inverse),
+            Err(e) => eprintln!("{}\n",e),
+        }
+    // undefined
 
 }
 
