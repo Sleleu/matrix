cc922226279f6b5e7c4fc003bf62a94c3f1433d9
Sebastien LELEU <leleu.sebastien@hotmail.fr>
2024-07-18 15:11:47 +0200

    add some tests

--- tests/ex06/ex06.rs
+++ tests/ex06/ex06.rs
@@ -3,7 +3,7 @@ use matrix::vector::Vector;
 
 fn cross_product_test() {
 
-    println!("{GREEN}Example tests{END}");
+    println!("\n\n{GREEN} CROSS PRODUCT TESTS {END}\n");
     
     let u = Vector::from([0., 0., 1.]);
     let v = Vector::from([1., 0., 0.]);
@@ -23,6 +23,36 @@ fn cross_product_test() {
     // [17.]
     // [-58.]
     // [-16.]
+
+    println!("\n\n{GREEN} OTHER TESTS {END}\n");
+
+    let u = Vector::from([0., 0., 0.]);
+    let v = Vector::from([0., 0., 0.]);
+    println!("{CYAN}Zero vectors{END}");
+    println!("{}", Vector::cross_product(&u, &v));
+    // [0.]
+    // [0.]
+    // [0.]
+
+    let u = Vector::from([1., 42.]);
+    let v = Vector::from([0., 42.]);
+    println!("{CYAN}Not in R3 = undefined{END}");
+    println!("{CYAN}G{GREEN}O{YELLOW} P{CYAN}A{GREEN}N{YELLOW}I{CYAN}I{GREEN}I{YELLOW}I{CYAN}C{END}");
+    println!("{CYAN}   ||||{END}");
+    println!("{CYAN}   ||||{END}");
+    println!("{CYAN}   ||||{END}");
+    println!("{CYAN}   ||||{END}");
+    println!("{CYAN}   ||||{END}");
+    println!("{CYAN}||||||||||{END}");
+    println!("{CYAN} ||||||||{END}");
+    println!("{CYAN}  ||||||{END}");
+    println!("{CYAN}   ||||{END}");
+    println!("{CYAN}    |||{END}");
+    println!("{}", Vector::cross_product(&u, &v));
+    // [0.]
+    // [0.]
+    // [0.]
+
 }
 
 fn main() {
